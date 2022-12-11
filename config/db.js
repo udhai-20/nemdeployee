b8b58b77e297c44e321e08cdbb4f2b6c685a6c46
udhai-20 <udhayaroger511@gmail.com>
2022-12-11 10:00:48 +0530

    a

--- config/db.js
+++ config/db.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 require("dotenv").config();
-mongoose.set("strictQuery", false);
 
+mongoose.set("strictQuery", false);
 const connection = mongoose.connect(process.env.MANGODB_URL);
 
 module.export = { connection };
