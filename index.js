ca5b0cfc0263ac0c5ceeb9cabef40e284ac4ff87
udhai-20 <udhayaroger511@gmail.com>
2022-12-11 10:15:07 +0530

    a

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const express = require("express");
+const { MongoClient } = require("mongodb");
 const { connection } = require("./config/db");
 const userRouter = require("./Routes/user.route");
 const notesRouter = require("./Routes/note.route");
 const { authentication } = require("./middleware/authentication");
 const cors = require("cors");
+const url = process.env.MANGODB_URL;
+const client = new MongoClient(url);
 const app = express();
 app.use(cors());
 app.use(express.json());
@@ -14,11 +17,22 @@ app.use("/users", userRouter);
 app.use(authentication);
 app.use("/notes", notesRouter);
 
-app.listen(8016, async () => {
-  try {
-    await connection;
-    console.log("port running the local mongodb http://localhost:8016/");
-  } catch (err) {
-    console.log("connection to db is failed");
+// app.listen(8016, async () => {
+//   try {
+//     await connection;
+//     console.log("port running the local mongodb http://localhost:8016/");
+//   } catch (err) {
+//     console.log("connection to db is failed");
+//   }
+// });
+
+client.connect((err) => {
+  if (err) {
+    console.error(err);
+    return false;
   }
+  // connection to mongo is successful, listen for requests
+  app.listen(8016, () => {
+    console.log("listening for requests");
+  });
 });
